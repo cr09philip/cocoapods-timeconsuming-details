a4725f85d9897e21a8ac9b9599f74673c3a80271
philip.lpf <philip.lpf@alibaba-inc.com>
2016-05-17 11:41:57 +0800

    add files to support 0.35 & 0.39 & 1.0.0

--- lib/cocoapods_plugin.rb
+++ lib/cocoapods_plugin.rb
@@ -1,2 +1,10 @@
 
-require 'cocoapods-timeconsuming-details.rb'
+if Pod::Version == '1.0.0'
+	require 'cocoapods_1.0.0-timeconsuming-details.rb'
+elsif Pod::Version == '0.39.0'
+	require 'cocoapods_0.39.0-timeconsuming-details.rb'
+elsif Pod::Version == '0.35.0'
+	require 'cocoapods_0.35.0-timeconsuming-details.rb'
+end
+
+	
