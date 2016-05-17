d03a9a79fe3a73cb1c90a134bfece4013a69ba1d
philip.lpf <philip.lpf@alibaba-inc.com>
2016-05-17 11:46:12 +0800

    modify Pod::VERSION

--- lib/cocoapods_plugin.rb
+++ lib/cocoapods_plugin.rb
@@ -1,9 +1,15 @@
 
-if Pod::Version == '1.0.0'
+if Pod::VERSION == '1.0.0'
+
+	puts "\033[32m hello world 1.0.0 \033[0m"  
 	require 'cocoapods_1.0.0-timeconsuming-details.rb'
-elsif Pod::Version == '0.39.0'
+elsif Pod::VERSION == '0.39.0'
+
+	puts "\033[32m hello world 0.39.0 \033[0m"  
 	require 'cocoapods_0.39.0-timeconsuming-details.rb'
-elsif Pod::Version == '0.35.0'
+elsif Pod::VERSION == '0.35.0'
+
+	puts "\033[32m hello world 0.35 \033[0m"  
 	require 'cocoapods_0.35.0-timeconsuming-details.rb'
 end
 
